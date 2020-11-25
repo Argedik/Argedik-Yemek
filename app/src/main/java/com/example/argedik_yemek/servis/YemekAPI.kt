b031e576b6fb148fa563434e83cb8c78c1bb0e98
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-25 16:16:41 +0300

    Update YemekAPI.kt

--- app/src/main/java/com/example/argedik_yemek/servis/YemekAPI.kt
+++ app/src/main/java/com/example/argedik_yemek/servis/YemekAPI.kt
@@ -8,21 +8,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface YemekAPI {
-    @GET("recipes/quickAnswer?q=How%20much%20vitamin%20c%20is%20in%202%20apples%3F")
+    @GET("Argedik/Argedik-Yemek/master/YemekJson.json")
     fun getYemek(): Single<List<Yemek>>
 
 
 }
 
-class YemekAPIServis{
-    private val BASE_URL ="https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-    private val api= Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build().create(YemekAPI::class.java)
-
-    fun getData():Single<List<Yemek>>{
-        return api.getYemek()
-    }
-}
