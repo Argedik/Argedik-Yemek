660577a644417d45524c79a2215e2c61afe5c22e
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 14:45:54 +0300

    Create YemekDAO.kt

---
+++ app/src/main/java/com/example/argedik_yemek/servis/YemekDAO.kt
@@ -0,0 +1,22 @@
+package com.example.argedik_yemek.servis
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.argedik_yemek.model.Yemek
+
+@Dao
+interface YemekDAO {
+    //Data Access Object
+    @Insert
+    suspend fun insertAll(vararg yemek:Yemek) : List<Long>
+
+    @Query("SELECT * FROM yemek")
+    suspend fun getAllYemek(): List<Yemek>
+
+    @Query("SELECT * FROM yemek WHERE uuid =:yemekId ")
+    suspend fun getYemek(yemekId:Int) :Yemek
+
+    @Query("DELETE FROM yemek")
+    suspend fun deleteAllYemek()
+}
