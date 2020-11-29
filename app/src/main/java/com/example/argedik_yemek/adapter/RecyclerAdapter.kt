d6cb0024a2a3dcee897f5c6b249833a49afbcdad
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 14:45:36 +0300

    Update RecyclerAdapter.kt

--- app/src/main/java/com/example/argedik_yemek/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/argedik_yemek/adapter/RecyclerAdapter.kt
@@ -3,13 +3,9 @@ package com.example.argedik_yemek.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.argedik_yemek.R
 import com.example.argedik_yemek.model.Post
-import com.example.argedik_yemek.model.Yemek
-import com.example.argedik_yemek.view.YemekListesiFragmentDirections
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recycler_row.view.*
 
@@ -39,38 +35,3 @@ class RecyclerAdapter(val postList:ArrayList<Post>) : RecyclerView.Adapter<Recyc
 
 }
 
-class YemekRecyclerAdapter(val yemekListesi:ArrayList<Yemek>):RecyclerView.Adapter<YemekRecyclerAdapter.YemekVH>(){
-    class YemekVH(itemView:View):RecyclerView.ViewHolder(itemView) {
-
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): YemekVH {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.recycler_row,parent,false)
-        return YemekVH(view)
-    }
-
-    override fun onBindViewHolder(holder: YemekVH, position: Int) {
-        println(position)
-        println(yemekListesi.get(position).Tarif)
-        println(yemekListesi.get(position).aisim)
-        holder.itemView.recycler_row_email.text=yemekListesi.get(position).aisim
-        holder.itemView.recycler_row_yorum.text=yemekListesi.get(position).Tarif
-        Picasso.get().load(yemekListesi[position].gorsel).into(holder.itemView.recycler_row_resim)
-        holder.itemView.setOnClickListener{
-            val action=YemekListesiFragmentDirections.actionYemekListesiFragmentToYemekDetayiFragment(0)
-            Navigation.findNavController(it).navigate(action)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return yemekListesi.size
-    }
-
-
-    fun YemekListesiGuncelle(yeniYemekListesi: List<Yemek>){
-        yemekListesi.clear()
-        yemekListesi.addAll(yeniYemekListesi)
-        notifyDataSetChanged()
-    }
-}
