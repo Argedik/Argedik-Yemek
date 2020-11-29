727158e5e5f2f87fc575eb7e49bf2cef59a24e43
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 14:46:13 +0300

    Update Sayfa2YemekDetayiVM.kt

--- app/src/main/java/com/example/argedik_yemek/viewmodel/Sayfa2YemekDetayiVM.kt
+++ app/src/main/java/com/example/argedik_yemek/viewmodel/Sayfa2YemekDetayiVM.kt
@@ -1,15 +1,21 @@
 package com.example.argedik_yemek.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.argedik_yemek.model.Yemek
+import com.example.argedik_yemek.servis.YemekDataBase
+import kotlinx.coroutines.launch
 
-class Sayfa2YemekDetayiVM : ViewModel() {
+class Sayfa2YemekDetayiVM(application: Application) : BaseViewModel(application) {
 
-    val besinLiveData = MutableLiveData<Yemek>()
+    val yemekLiveData = MutableLiveData<Yemek>()
 
-    fun roomVerisiniAl(){
-        val deneme= Yemek("deneme","malzeme","sosu","2. sos mu olur","boşver")
-        besinLiveData.value= deneme
+    fun roomVerisiniAl(uuid : Int){
+        launch {
+            val dao = YemekDataBase(getApplication()).yemekDao()
+            val yemek = dao.getYemek(uuid)
+            yemekLiveData.value = yemek
+        }
     }
 }
