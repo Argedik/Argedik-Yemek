17caccd5408ab703be20771da430d69a39b22f10
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 19:15:31 +0300

    Update YemekDataBase.kt

--- app/src/main/java/com/example/argedik_yemek/servis/YemekDataBase.kt
+++ app/src/main/java/com/example/argedik_yemek/servis/YemekDataBase.kt
@@ -9,18 +9,14 @@ import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.argedik_yemek.model.Yemek
 
-//Database ile ilgili bir değişiklik yaptığımızda versiyonu değiştirmemiz gerekiyor
 @Database(entities = arrayOf(Yemek::class),version=4)
 abstract class YemekDataBase : RoomDatabase() {
     abstract fun yemekDao():YemekDAO
 
-    //Singleton
     companion object{
         @Volatile private var instance: YemekDataBase? = null
         private val lock =Any()
 
-        //instance null değilse instance döndür. Eğer oluşturulmadıysa synchronized başlat ve dayabase oluştur
-        //also: bunu yap üstüne şunuda yap
         operator fun invoke(context:Context) = instance?: synchronized(lock){
             instance?: databaseOlustur(context).also {
                 instance = it
@@ -31,17 +27,11 @@ abstract class YemekDataBase : RoomDatabase() {
             context.applicationContext,
             YemekDataBase::class.java,"yemekdatabase").build()
 
-
         @JvmField
         @VisibleForTesting
-        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+        val MIGRATION_3_4: Migration = object : Migration(3, 4) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                // Room uses an own database hash to uniquely identify the database
-                // Since version 1 does not use Room, it doesn't have the database hash associated.
-                // By implementing a Migration class, we're telling Room that it should use the data
-                // from version 1 to version 2.
-                // If no migration is provided, then the tables will be dropped and recreated.
-                // Since we didn't alter the table, there's nothing else to do here.
+                //database.execSQL("Yemek bsos")
             }
         }
 
@@ -51,14 +41,11 @@ abstract class YemekDataBase : RoomDatabase() {
                 if (instance == null) {
                     instance = Room.databaseBuilder(context.applicationContext,
                         YemekDataBase::class.java, "yemekdatabase")
-                        .addMigrations(MIGRATION_1_2)
+                        .addMigrations(MIGRATION_3_4)
                         .build()
                 }
                 return instance
             }
         }
     }
-
-
-
 }
