6212d38cbc48ba18b814619f7d0aefedcab6eeac
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 14:46:05 +0300

    Update YemekDetayiFragment.kt

--- app/src/main/java/com/example/argedik_yemek/view/YemekDetayiFragment.kt
+++ app/src/main/java/com/example/argedik_yemek/view/YemekDetayiFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.argedik_yemek.R
+import com.example.argedik_yemek.databinding.FragmentYemekDetayiBinding
+import com.example.argedik_yemek.util.gorselIndir
+import com.example.argedik_yemek.util.placeHolderYap
 import com.example.argedik_yemek.viewmodel.Sayfa2YemekDetayiVM
 import kotlinx.android.synthetic.main.fragment_yemek_detayi.*
 
 class YemekDetayiFragment : Fragment() {
 
     private lateinit var viewModel : Sayfa2YemekDetayiVM
-
+    private lateinit var dataBinding:FragmentYemekDetayiBinding
 
     private var yemekId=0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,30 +31,32 @@ class YemekDetayiFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_yemek_detayi, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_yemek_detayi,container,false)
+        //return inflater.inflate(R.layout.fragment_yemek_detayi, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        viewModel = ViewModelProviders.of(this).get(Sayfa2YemekDetayiVM::class.java)
-        viewModel.roomVerisiniAl()
-
-
-
         arguments?.let {
             yemekId = YemekDetayiFragmentArgs.fromBundle(it).YemekId
-            println(yemekId)
         }
+        viewModel = ViewModelProviders.of(this).get(Sayfa2YemekDetayiVM::class.java)
+        viewModel.roomVerisiniAl(yemekId)
+
         observerLivaData()
     }
     fun observerLivaData(){
-        viewModel.besinLiveData.observe(viewLifecycleOwner, Observer {yemek->
+        viewModel.yemekLiveData.observe(viewLifecycleOwner, Observer { yemek->
             yemek?.let{
-                ilkmetin.text=it.aisim
+                dataBinding.secilenYemek =it
+                /*ilkmetin.text=it.aisim
                 ikincimetin.text=it.amalzeme
                 ucuncumetin.text=it.asos
                 besincimetin.text=it.amalzeme
+                context?.let {
+                    liste_Detay_Resmi.gorselIndir(yemek.gorsel, placeHolderYap(it))
+                }*/
             }
         })
     }
