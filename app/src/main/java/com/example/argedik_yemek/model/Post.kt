ce5fbc48d4455501a9b80d248dcfe39291a7f8aa
Enes Yasin Gedik <72869669+Argedik@users.noreply.github.com>
2020-11-29 14:45:49 +0300

    Update Post.kt

--- app/src/main/java/com/example/argedik_yemek/model/Post.kt
+++ app/src/main/java/com/example/argedik_yemek/model/Post.kt
@@ -1,21 +1,4 @@
 package com.example.argedik_yemek.model
 
-import com.google.gson.annotations.SerializedName
-
-
-data class Yemek(
-    @SerializedName("aisim")
-    val aisim : String?,
-    @SerializedName("amalzeme")
-    val amalzeme : String?,
-    @SerializedName("asos")
-    val asos : String?,
-    @SerializedName("gorsel")
-    val gorsel : String?,
-    @SerializedName("Tarif")
-    val Tarif : String?,
-){
-
-}
 
 class Post (var kullaniciEmail: String, var kullaniciYorum:String, var gorselUrl:String)
